07a8c2dc7c589a877f4d6c8fcfcd19822181c5c3
asapupravallika <asapupravallika@gmail.com>
2026-09-09 23:31:50 +0530

    EventHub Backend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import api from "../services/api";
 
 function Navbar() {
   const navigate = useNavigate();
